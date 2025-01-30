5fc2abac3257d480c5011d60287267515e936461
KrisKlindt <kklindt@arizona.edu>
2025-01-30 14:57:36 -0800

    Lab 1 completed

--- Labs/Lab1/Lab1.py
+++ Labs/Lab1/Lab1.py
@@ -62,7 +62,13 @@ RoSparke = 7.9 *u.kpc # Sparke and Gallagher Text
 VLSR_Reid = VLSR(RoReid)
 print(VLSR_Reid)
 
+#Compute VLSR using Gravtiy Collab
+VLSR_Abuter = VLSR(RoAbuter)
+print(np.round(VLSR_Abuter))
 
+#Compute VLSR using Sparke and Gallagher
+VLSR_Sparke = VLSR(RoSparke)
+print(VLSR_Sparke)
 
 # ### b)
 # 
@@ -70,19 +76,34 @@ print(VLSR_Reid)
 # 
 # Note that 1 km/s $\sim$ 1kpc/Gyr
 
+# Orbital period = 2piR/v
+def TorbSun(Ro, Vc):
+    """
+    Function that computes the orbital period of the sun
+    T = 2 pi R / V
+    Inputs:
+        Ro: (astropy quantity) distance to the galactic center from the sun (kpc)
+        Vc: (astropy quantity) velocity of the sun in the "v" direction (km/s)
+    Outputs:
+        T: (astropy quantity) Orbital period (Gyr)
+    """
+    VkpcGyr = Vc.to(u.kpc/u.Gyr) # converting V to kpc/Gyr
+    T = 2*np.pi*Ro / VkpcGyr # Orbital period
+    return T
 
-
-
-
+VsunPec = 12.24*u.km/u.s # Peculiar motion
+Vsun = VLSR_Abuter + VsunPec # Total motion of the sun in the "v" direction
+    
+#Orbital Period of the Sun
+T_Abuter = TorbSun(RoAbuter,Vsun)
+print(np.round(T_Abuter, 3))
 
 # ### c)
 # 
 # Compute the number of rotations about the GC over the age of the universe (13.8 Gyr)
 
-
-
-
-
+AgeUniverse = 13.8 * u.Gyr
+print(AgeUniverse/T_Abuter)
 
 # ## Part B  Dark Matter Density Profiles
 # 
@@ -104,9 +125,41 @@ print(VLSR_Reid)
 # What about at 260 kpc (in units of  M$_\odot$) ? 
 
 
+Grav = const.G.to(u.kpc**3 / u.Gyr**2 / u.Msun)
 
+# Density Profile rho = VLSR^2 / (4*pi*G*R^2)
+# Mass (r) = Integrate rho dV
+#          = Integrate rho 4*pi*r^2*dr
+#          = Integrate VLSR^2 / 4*pi*r*2 *4*pi*r^2*dr
+#          = Integrate VLSR^2/G dr
+#          = VLSR^2/G * r
 
+def massIso(r,VLSR):
+    """
+    This function will compute the dark matter mass enclosed within a given 
+    distance, r, assuming an Isothermal Sphere Model
+    M(r) = VLSR^2/G * r
+    Inputs:
+        r : (astropy quantity) distancce from the galactic center (kpc)
+        VLSR : (astropy quantity) the velocity at the Local Standard of Rest (km/s)
+    Outputs:
+        M: (astropy quantity) mass enclosed within r (Msun)
+    """
+    
+    VLSRkpcGyr = VLSR.to(u.kpc/u.Gyr) # translating to kpc/Gyr
+    
+    M = VLSRkpcGyr**2/Grav*r # Isothermal Sphere Mass Profile
+    
+    return M
 
+# Compute mass enclosed within Ro using Gravity Collab
+mIsoSolar = massIso(RoAbuter, VLSR_Abuter)
+print(mIsoSolar)
+print(f"{mIsoSolar:.2e}")
+
+# Compute mass enclosed within 260 kpc
+mIso260 = massIso(260*u.kpc, VLSR_Abuter)
+print(f"{mIso260:.2e}")
 
 # ## c) 
 # 
@@ -123,7 +176,38 @@ print(VLSR_Reid)
 # 
 # How does this compare to estimates of the mass assuming the Isothermal Sphere model at 260 kpc (from your answer above)
 
+# Potential for a Hernquist Sphere
+# Phi = -G*M/(r+a)
 
+# Escape Speed becomes:
+# vesc^2 = 2*G*M/(r+a)
 
+# rearrange for M
+# M = vesc^2/2/G*(r+a)
 
+def massHernVesc(vesc, r, a=30*u.kpc):
+    """
+    This function determines the total dark matter mass needed given an escape speed,
+    assuming a Hernquist profile
+    M = vesc^2/2/G*(r+a)
+    Inputs:
+        vesc : (astropy quantity) escape speed (or speed of satellite) (km/s)
+        a : (astropy quantity) the Hernquist scale length (kpc)
+            default value of 30 kpc
+        r : (astropy quantity) distance from the galactic center (kpc)
 
+    Outputs:
+        M (astropy quantity) mass within r (Msun)
+    """
+
+    vescKpcGyr = vesc.to(u.kpc/u.Gyr) # translate to kpc/Gyr
+    
+    M = vescKpcGyr**2/2/Grav*(r+a)
+
+    return M
+
+Vleo = 196*u.km/u.s # Speed of Leo I Sohn et al.
+r = 260*u.kpc
+
+MLeoI = massHernVesc(Vleo, r)
+print(f"{MLeoI:.2e}")
